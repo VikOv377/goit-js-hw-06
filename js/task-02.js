77ceda3f6e8fa2ecd2908ef928ba8d3a1d99dc1f
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-09-11 17:28:09 +0200

    Done task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients = document.querySelector('#ingredients');
+const ingredientsArray = [];
+
+ingredients.forEach(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.className = 'item';
+
+    ingredientsArray.push(item);
+});
+
+listOfIngredients.append(...ingredientsArray);
+
+
+
+
