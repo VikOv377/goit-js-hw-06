faee3ee5fd229d128aa57f3fb5186fc755c0372a
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-09-12 12:08:51 +0200

    Done task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const inputForm = document.querySelector('.login-form');
+
+inputForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const formEmail = formElements.email.value;
+    const formPassword = formElements.password.value;
+
+    if (formEmail === '' || formPassword === '') {
+        return alert ('Please fill up all the fields!');
+    };
+
+    const userData = {Email: formEmail, Password: formPassword};
+    console.log(userData);
+
+    event.currentTarget.reset();
+});
+
 
