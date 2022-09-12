69c308d659d1ceb716e15deec75f61d2410febdc
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-09-12 16:46:15 +0200

    Done task #10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,46 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector('#controls > input');
+const buttonCreate = document.querySelector('[data-create]');
+const buttonDestroy = document.querySelector('[data-destroy]');
+const boxesApp = document.querySelector('#boxes');
+
+let amount = inputNumber.value;
+
+function createBoxes (amount) {
+  const boxesToAppear = [];
+
+      for (let i = 0; i < amount; i += 1) {
+        const div = document.createElement('div');
+        console.log(div);
+
+        div.style.height = `${30 + 10 * i}px`;
+		    div.style.width = `${30 + 10 * i}px`;
+		    div.style.background = getRandomHexColor();
+		    
+        boxesToAppear.push(div);
+      };
+
+      return boxesToAppear;
+};
+
+buttonCreate.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(inputNumber.value);
+  boxesApp.append(...boxesToAdd); 
+}); 
+
+buttonDestroy.addEventListener('click', () => {
+  boxesApp.innerHTML = '';
+  
+  if (inputNumber !== '') {
+      inputNumber = '';
+    };
+});
+
+
+
+
+
+
