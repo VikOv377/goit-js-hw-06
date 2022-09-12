c832befd4001624bb9ac2cf37d417e29b045bb28
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-09-12 12:37:53 +0200

    Done task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const buttonForChangeColor = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+buttonForChangeColor.addEventListener('click', event => {
+  let randomColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
